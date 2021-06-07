ef2b4a05b6e31d41859fd501b6b2abb95c45d66d
Waqas Hafeez <waqas.hafeez@virtualforce.io>
2021-06-07 16:07:30 +0500

    adding admin module user changes and listing with customisation

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Testing databse entries."""
+
+    def test_create_user_with_email_successful(self):
+        """Test user creation with email address and password"""
+        email = "whafeez21@gmail.com"
+        password = "Password1"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_user_email_normalise(self):
+        """Testing if the email address of the user is normalised or not"""
+        email = "whafeez21@GMAIL.COM"
+        user = get_user_model().objects.create_user(email, '12345')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_valid_email_address(self):
+        """Testing if user have entered email address or not"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', '12345')
+
+    def test_create_news_super_user(self):
+        """Testing creation of new super user"""
+        user = get_user_model().objects.create_superuser(
+            'whafeez22@gmail.com',
+            '12345'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
